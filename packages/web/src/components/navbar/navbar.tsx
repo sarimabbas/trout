fad2141ee0496ef1835fd7448d1d7bb7189bcbb1
Sarim Abbas <sarim.stays@gmail.com>
2023-07-07 00:17:12 -0700

    wip

--- packages/web/src/components/navbar/navbar.tsx
+++ packages/web/src/components/navbar/navbar.tsx
@@ -11,7 +11,9 @@ export const Navbar = () => {
   return (
     <div className="container flex items-center justify-between gap-4 p-8 mx-auto bg-gray-50 dark:bg-zinc-900">
       <div className="flex items-center gap-12">
-        <div className="text-2xl font-semibold">🐟 Trout</div>
+        <Link href="/" className="text-2xl font-semibold">
+          🐟 Trout
+        </Link>
         <OrganizationSwitcher
           appearance={theme === "dark" ? { baseTheme: dark } : {}}
         />
