fdf31875e8dc38db428d793fe4308656bef33767
Sarim Abbas <sarim.stays@gmail.com>
2023-07-08 19:56:12 -0700

    wip

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,30 +1,34 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: "🐟 Trout",
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: "https://github.com/withastro/starlight",
       },
       sidebar: [
         {
-          label: 'Guides',
+          label: "Quick start",
+          link: "/",
+        },
+        {
+          label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            { label: "Example Guide", link: "/guides/example/" },
           ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: "Reference",
+          autogenerate: { directory: "reference" },
         },
       ],
     }),
   ],
 
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
 });
