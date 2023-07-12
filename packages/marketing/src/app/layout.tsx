c7f3d4f7b0c415e435a624ebd750f9a135d75418
Sarim Abbas <sarim.stays@gmail.com>
2023-07-11 22:35:09 -0700

    wip

--- packages/marketing/src/app/layout.tsx
+++ packages/marketing/src/app/layout.tsx
@@ -1,13 +1,15 @@
-import "./globals.css";
 import "@sarim.garden/ui/css";
 import type { Metadata } from "next";
-import { Bungee, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Trout",
   description: "Easy webhook development",
+  icons:
+    "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐟</text></svg>",
 };
 
 export default function RootLayout({
