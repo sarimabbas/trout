cb26cce72cf454eda1a3f0e9315e2e6dfe153541
Sarim Abbas <sarim.stays@gmail.com>
2023-07-11 22:38:46 -0700

    wip

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -8,6 +8,15 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐟</text></svg>",
+          },
+        },
+      ],
       title: "🐟 Trout",
       social: {
         github: "https://github.com/sarimabbas/trout",
