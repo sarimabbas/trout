5a31ba978ddfe19e0ead40ad6e2dba48faef5c0e
Sarim Abbas <sarim.stays@gmail.com>
2023-07-12 21:26:39 -0700

    wip

--- packages/web/src/components/navbar/navbar.tsx
+++ packages/web/src/components/navbar/navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
   const { theme } = useTheme();
 
   return (
-    <div className="flex flex-wrap items-center justify-between gap-8 p-8 bg-white border-4 border-blue-200 shadow-md shadow-blue-50 dark:bg-zinc-900 rounded-xl">
+    <div className="flex flex-wrap items-center justify-between gap-8 p-8 bg-white border-4 border-blue-200 shadow-md dark:border-sky-400 shadow-blue-50 dark:shadow-sky-800 dark:bg-zinc-900 rounded-xl">
       {/* left */}
       <div className="flex flex-wrap items-center gap-8">
         <Link
