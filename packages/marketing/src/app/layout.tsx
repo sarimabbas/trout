f20b18c1797ac6844c48ab903b4d17a6041216f0
Sarim Abbas <sarim.stays@gmail.com>
2023-07-12 20:31:07 -0700

    add analytics to marketing site

--- packages/marketing/src/app/layout.tsx
+++ packages/marketing/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@sarim.garden/ui/css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
