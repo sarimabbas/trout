fba48a56f997b7816e725f49814639ba24b93ddb
Sarim Abbas <sarim.stays@gmail.com>
2023-07-29 14:35:22 -0700

    update description

--- packages/marketing/src/app/layout.tsx
+++ packages/marketing/src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Trout",
-  description: "Easy webhook development",
+  description: "Webhooks made easy",
   icons:
     "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐟</text></svg>",
 };
