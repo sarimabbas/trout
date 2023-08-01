99f7d32871afbd865e172c994b159b907b212869
Sarim Abbas <sarim.stays@gmail.com>
2023-07-31 22:11:43 -0700

    test cronjob

--- packages/web/src/app/api/v0/waitlist/offboard/route.ts
+++ packages/web/src/app/api/v0/waitlist/offboard/route.ts
@@ -6,7 +6,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // runs as a cron job everyday via vercel.json
-export async function POST() {
+export async function GET() {
   // get access token
   const accessToken = await waitlist.getAccessToken();
 
@@ -20,20 +20,22 @@ export async function POST() {
   // find top waitlistees
   const waitlistees = await waitlist.getWaitlistees(accessToken, limit);
 
-  // offboard from waitlist
-  const offboarded = await waitlist.offboardWaitlistees(
-    accessToken,
-    waitlistees.map((w) => w.uuid)
-  );
+  console.log({ waitlistees });
 
-  // send them an email
-  const resendResponse = await resend.emails.send({
-    from: "Trout <hi@updates.trout.run>",
-    reply_to: "Trout <hi@trout.run>",
-    to: offboarded.map((w) => w.email),
-    subject: "Your invitation to Trout",
-    react: WaitlistOffboardEmail({}),
-  });
+  // // offboard from waitlist
+  // const offboarded = await waitlist.offboardWaitlistees(
+  //   accessToken,
+  //   waitlistees.map((w) => w.uuid)
+  // );
+
+  // // send them an email
+  // const resendResponse = await resend.emails.send({
+  //   from: "Trout <hi@updates.trout.run>",
+  //   reply_to: "Trout <hi@trout.run>",
+  //   to: offboarded.map((w) => w.email),
+  //   subject: "Your invitation to Trout",
+  //   react: WaitlistOffboardEmail({}),
+  // });
 
   if (process.env.RUNTIME_ENV === "production") {
     try {
@@ -44,7 +46,7 @@ export async function POST() {
         },
         body: JSON.stringify({
           status: "success",
-          message: `Offboarded ${offboarded.length} waitlistees`,
+          message: `Offboarded ${0} waitlistees`,
         }),
       });
     } catch (e) {
@@ -52,5 +54,5 @@ export async function POST() {
     }
   }
 
-  return NextResponse.json({ resendResponse });
+  return NextResponse.json({ hello: "world" });
 }
